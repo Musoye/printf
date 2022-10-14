4337d8d3a302307db888fadb72a732dff58c900d
buzo22 <chibuzoakana@yahoo.com>
2022-10-14 15:25:28 -0700

    length of string

---
+++ _strlen.c
@@ -0,0 +1,19 @@
+
+
+
+
+
+/**
+ * _strlen - length of a string
+ * @s: the string
+ * Return: the length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (0);
+}
